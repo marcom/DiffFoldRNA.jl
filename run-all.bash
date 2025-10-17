3901102e9dc64b565033e0d94b06204831975e41
Marco Matthies <71844+marcom@users.noreply.github.com>
2025-10-17 04:21:11 +0200

    Tweak run-all.bash script

--- run-all.bash
+++ run-all.bash
@@ -10,4 +10,6 @@ cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 julia --project=. -e 'import Pkg; Pkg.test()'
 
 # validation scripts
-julia --project=validation check-seqstruct-partition-All1Model-vienna-vs-nussinov.jl
+julia --project=validation validation/check-seqstruct-partition-All1Model-vienna-vs-nussinov.jl
+#julia --project=validation validation/check-seqstruct-partition-nussinov-vs-bruteforce.jl
+#julia --project=validation validation/check-seqstruct-partition-vienna-vs-bruteforce.jl
